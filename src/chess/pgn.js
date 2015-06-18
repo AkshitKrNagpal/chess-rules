0e69b7cff0c63ecbed8dba3ac932ee95056d03d3
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-06-18 15:05:13 +0200

    Prepare PGN module for move/PGN conversion.

---
+++ src/chess/pgn.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function pgnToMove(position, pgnMove) {
+    return null;
+}
+
+function moveToPgn(position, move) {
+    return null;
+}
+
+module.exports = {
+    pgnToMove: pgnToMove,
+    moveToPgn: moveToPgn
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@
 var position = require('./chess/position');
 var moves = require('./chess/moves');
 var updates = require('./chess/updates');
+var pgn = require('./chess/pgn');
 
 var chessRules = {
     getInitialPosition: position.initialPositionFactory,
     getAvailableMoves: moves.getAvailableMoves,
-    applyMove: updates.applyMove
+    applyMove: updates.applyMove,
+    pgnToMove: pgn.pgnToMove,
+    moveToPgn: pgn.moveToPgn
 };
 
 module.exports = chessRules;
