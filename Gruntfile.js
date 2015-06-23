1003c54f487e225a78973329458d7911f1d4c8da
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2015-06-23 11:03:15 +0200

    Renamed the alias module.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
     browserify: {
         options: {
             alias: {
-                'rules': './src/index.js'
+                'chess-rules': './src/index.js'
             }
         },
         dist: {
